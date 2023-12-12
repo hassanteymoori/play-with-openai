2fccb8fd1325768ca86d7df38eff22c332603929
Hassan Teymoori <hassan.teymoori1372@gmail.com>
2023-12-12 15:10:43 +0100

    poet message and json response

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $response = Http::withToken(config('services.openai.secret'))
+        ->post(config('services.openai.url'),
+            [
+                "model" => "gpt-3.5-turbo",
+                "messages" => [
+                    [
+                        "role" => "system",
+                        "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => "Compose a poem that explains the concept of recursion in programming."
+                    ]
+                ]
+            ])->json();
+
+    dd($response);
 });
